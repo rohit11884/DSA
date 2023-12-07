6e8ce820426d64c5553f1cb837c30e69b9b9082b
Rohit <rohit11884kr@gmail.com>
2023-12-07 14:11:20 +0530

    sorting of an array

---
+++ creating_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class sort{
+    public void code(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int [n];
+        System.out.println("Enter the element in array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Elements in the array is ");
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+public class creating_array{
+    public static void main(String[] args) {
+        sort so=new sort();
+        so.code();
+    }
+}
---
+++ selection_sort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class selection_sort {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of array");
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            System.out.println("Enter the element in array");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n - 1; i++) {
+
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[i]) {
+                        int temp = 0;
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+
+            }
+            System.out.println("Sorted array is ");
+            for (int i = 0; i < n; i++) {
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
