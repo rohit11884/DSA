7e815b0cb2f7aeb31973f4597d2dd9c468605dd2
Rohit <rohit11884kr@gmail.com>
2023-12-07 18:27:30 +0530

    sorting

---
+++ bubble_sorting.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class bubble_sorting {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of array");
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            System.out.println("Enter the element in array");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int counter=1;
+            while (counter<n-1) {
+                 for(int i=0;i<n-counter;i++){
+                    if(arr[i]>arr[i+1]){
+                        int temp=arr[i];
+                         arr[i]=arr[i+1];
+                         arr[i+1]=temp;
+                    }
+                    
+                 }
+                 counter++;
+            }
+            System.out.println("Sorted array is");
+            for(int i=0;i<n;i++){
+                System.out.println(arr[i]);
+            }
+        }
+       
+    }
+}
--- creating_array.java
+++ creating_array.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 class sort{
     public void code(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int n=sc.nextInt();
-        int arr[]=new int [n];
-        System.out.println("Enter the element in array");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("Elements in the array is ");
-        for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of array");
+            int n=sc.nextInt();
+            int arr[]=new int [n];
+            System.out.println("Enter the element in array");
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            System.out.println("Elements in the array is ");
+            for(int i=0;i<n;i++){
+                System.out.println(arr[i]);
+            }
         }
     }
 }
